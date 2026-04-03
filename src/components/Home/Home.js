d673a1aaa1b00ad90187a63d4e6b069eb9a6a96c
Matt <mburtonshaw@gmail.com>
2026-04-02 23:34:47 -0400

    initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.css"; // import the component-specific CSS
+
+function Home() {
+  return (
+    <div className="home_container">
+      <h1 className='text-center m-4'>Hello World</h1>
+    </div>
+  );
+}
+
+export default Home;
