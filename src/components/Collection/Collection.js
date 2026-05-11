73a8be1f311efd98321ef5b6ce3e6ca59b1b9134
Matt <mburtonshaw@gmail.com>
2026-05-11 17:31:37 -0400

    styling

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -48,7 +48,7 @@ function Collection() {
 
   return (
     <div className="collection_container mt-4 p-5">
-      <div className="card collection_card p-3">
+      <div className="card collection_card p-5">
       <div className="row justify-content-center">{todoMapper()}</div>
       </div>
     </div>
