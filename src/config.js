5bf1adf3c72940b36655a38600c54e2aa28a47e7
Matt <mburtonshaw@gmail.com>
2026-05-27 18:36:49 -0400

    production config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-//  const config = {
-//      apiBaseUrl: 'https://todo-v3-backend-3f9acbb0d16b.herokuapp.com/api',
-//    };
+ const config = {
+     apiBaseUrl: 'https://todo-v3-backend-3f9acbb0d16b.herokuapp.com/api',
+   };
 
-  const config = {
-    apiBaseUrl: 'http://localhost:8081/api',
-  };
+  // const config = {
+  //   apiBaseUrl: 'http://localhost:8081/api',
+  // };
 
 export default config;
