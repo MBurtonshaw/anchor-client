e309c286d6d74df3f92e47eeea34084899ab1c2c
Matt <mburtonshaw@gmail.com>
2026-06-03 22:17:24 -0400

    small changes in UI

--- src/config.js
+++ src/config.js
@@ -2,6 +2,8 @@
      apiBaseUrl: 'https://todo-v4-d9b25ee3e4de.herokuapp.com/api',
    };
 
-
+  // const config = {
+  //   apiBaseUrl: 'http://localhost:8081/api',
+  // };
 
 export default config;
