c5ce5168f1d2c1077075808e67f0d49c662bf202
Matt <mburtonshaw@gmail.com>
2026-06-10 00:31:17 -0400

    split contexts and services for weekendTask

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
-//  const config = {
-//      apiBaseUrl: 'https://anchor-v5-backend-6d64774f4c73.herokuapp.com/api',
-//    };
+ const config = {
+     apiBaseUrl: 'https://anchor-v5-backend-6d64774f4c73.herokuapp.com/api',
+   };
 
-  const config = {
-    apiBaseUrl: 'http://localhost:8081/api',
-  };
+
 
 export default config;
